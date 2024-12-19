8b67c520aa0cecc3af963fcec8fca56081dc5195
tedo380 <nagervadzetevdore7@gmail.com>
2024-12-19 09:08:35 -0800

    second commit

---
+++ level 20/classwork/classwork.py
@@ -0,0 +1,16 @@
+for i in range(10):
+    print("tevdore nagervadze")
+
+for i in range(0, 21, 2):
+    print(i)
+
+for i in range(11, 40, 2):
+    print(i)
+
+range1 = range(10, 31, 2)
+numbers_sum = 0
+
+
+for i in range1:
+    numbers_sum = numbers_sum + i
+print(numbers_sum)
---
+++ level 20/homework/homework.py
@@ -0,0 +1,26 @@
+# for ციკლს შეუძლია ერთი მაგალითის რამდენიმეჯერ დაწერა
+# საიტერაციო ცვლადი გადის რაიმე გარკვეულ მიმდევრობას სანამ ყველაფერი შესრულდება
+# კოდის ბლოკით ჩვენ შეგვიძლია გამოვივიყენოთ რამდენიმეჯერ კოდი
+
+
+for i in range(10, 30):
+    print(i)
+
+for i in range(20, 41):
+    print(i)
+
+for i in range(50, 61, 2):
+    print(i)
+
+for i in range(11, 51, 2):
+    print(i)
+
+num = 0
+for i in range(10, 20):
+    num = num + i
+    print(num)
+
+num1 = 0
+for i in range(2, 31, 2):
+    num1 = num1 + i
+    print(num1)
