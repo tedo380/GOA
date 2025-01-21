d05327f49bbdbf8148605ca4d5f5a1de65b8194f
tedo380 <nagervadzetevdore7@gmail.com>
2025-01-21 01:35:03 -0800

    second commit

---
+++ Level 30/classwork/classwork.py
@@ -0,0 +1,43 @@
+my_list = ["apple", 3.14, 42, True, "banana", False, 7.89, 100, [1, 2, 3], [4, 5, 6]]
+sliced_list_2_numbers = my_list[3:7]
+print(sliced_list_2_numbers)
+
+
+sliced_list_3_numbers = my_list[2:8:2]
+print(sliced_list_3_numbers)
+
+
+
+my_list = [i for i in range(1, 16)]
+print(my_list)
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+reversed_list = my_list[::-1]
+print(reversed_list)
+[15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+print(my_list[1:6])  
+print(my_list[4:10])  
+print(my_list[:5]) 
+print(my_list[7:])  
+print(my_list[3:12])  
+print(my_list[1:10:2])  
+print(my_list[::3])  
+print(my_list[2:15:4])  
+print(my_list[5::5])  
+print(my_list[:15:3])  
+print(my_list[1:])  
+print(my_list[:10])  
+print(my_list[::])  
+print(my_list[5:])  
+print(my_list[:]) 
+
+
+
+
+my_list = [i for i in range(1, 11)]
+print(my_list)
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(my_list[-9:-4])  
+print(my_list[-7:-2])  
+print(my_list[-10:-5])  
+print(my_list[-6:])  
+print(my_list[:-5])
---
+++ Level 30/homework/homework.py
@@ -0,0 +1,38 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+first_five = my_list[:5]  
+
+my_string = "Hello, World!"
+last_three = my_string[-3:]
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+every_second = my_list[::2]
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+every_third = my_list[::3]
+
+my_list = [1, 2, 3, 4, 5]
+last_element = my_list[-1]
+
+my_string = "Hello, World!"
+slice_excluding_first_last = my_string[1:-1]
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+middle_to_end = my_list[len(my_list)//2:]
+
+my_list = [1, 2, 3, 4, 5]
+list_copy = my_list[:]
+
+my_string = "Hello, World!"
+reversed_string = my_string[::-1]
+ 
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+first_half = my_list[:len(my_list)//2]
+
+my_list = [1, 2, 3, 4, 5, 6]
+exclude_first_three = my_list[3:]
+
+my_string = "Hello, World!"
+portion_string = my_string[1:-1]  
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+last_five = my_list[-5:]
